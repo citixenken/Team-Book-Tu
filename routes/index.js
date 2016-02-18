f740efbd16289349a15d31417c43446ad7f594af
xor <xor@mail.com>
2016-02-18 10:09:11 +0300

    fine-tuning urls

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,44 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team-Book-Tu' }); /*Team-Book-Tu */
+  //res.render('index', { title: 'Team-Book-Tu' }); /*Team-Book-Tu */
+  res.render('index', { user : req.user });
+  
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Team-Book-Tu' }); /*Team-Book-Tu */
+  //res.render('home', { user : req.user });
+  
+});
+
+router.get('/register' , function(req, res){
+	res.render('register', {});
+});
+
+router.post('/register', function(req, res){
+	Account.register(new Account({ username : req.body.username}), req.body.password, function(err, account){
+		if (err){
+			return res.render('register', { account : account });
+		}
+		passport.authenticate('local')(req, res, function(){
+			res.redirect('/');
+		});
+	});
+});
+
+router.get('/login', function(req, res){
+	res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res){
+	res.redirect('/');
+	//res.render('index', { title: 'Team-Book-Tu' }); /*Team-Book-Tu */
+});
+
+router.get('/logout', function(req, res){
+	req.logout();
+	res.redirect('/');
 });
 
 
