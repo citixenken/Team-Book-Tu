3a40a0bf9e3e375d6fe59cc8d627eaf0eaba53b9
citizen-ken <citizen-ken@users.noreply.github.com>
2016-03-07 09:01:44 +0300

    search issues/not resolved

--- routes/index.js
+++ routes/index.js
@@ -76,7 +76,26 @@ router.get('/search', function(req, res, next){
   });
 });
 
-router.post('/search', function(req, res){
+router.post('/search', function(req, res,next){
+  var query = {};
+  //console.log(req.body);
+  for (var k in req.body){
+    if ('_csrf' === k) continue;
+
+    var v = req.body[k];
+    if (v) query[k] = RegExp(v, 'i');
+  }
+  //console.log(query);
+  //query.userId= req.user.id;
+  Book.find(query, function(err, docs){
+    if (err) return next(err);
+
+    if (docs.length === 0){
+      req.flash('errors', { msg: 'No book matching that criteria is in our database.'});
+    }
+    
+    res.render('search', { searches: docs });
+  });
   
 });
 
